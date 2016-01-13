a34d22ea61110d337ed2cb78cde59561ee490353
ktorz <matthias.benkort@gmail.com>
2016-01-13 22:13:20 +0100

    Update backend: remove object and use of plain list instead such that the insertion order is preserved

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,15 +6,33 @@ export const ErrAlreadyInGarage = new Error("Vehicle already in the garage")
 
 // ----- Garage
 export function Garage (levels) {
-    this.db = {}
+    this.db = []
     this.levels = levels
 }
 
 /** Entry: { vehicle: Vehicle, spot: { place: Number, floor: Number } } */
 
-/** Vehicle -> Error | Entry */
+/** [Entry], String -> Maybe(Entry) */
+function find(xs, license) {
+    return xs.reduce((res, x) => res || x.vehicle.license === license && x, false) || null
+}
+
+/** [Entry], String -> Maybe(Number) */
+function findIndex(xs, license) {
+    let index = xs.reduce((res, x, i) => res || x.vehicle.license === license && i+1, false)
+    return !index ? null : index - 1
+}
+
+/** [Entry], entry -> () */
+function remove(xs, x) {
+    let index = xs.indexOf(x)
+    if (index === -1) { return }
+    xs.splice(index, 1)
+}
+
+/** Vehicle -> Either(Error, Entry) */
 Garage.prototype.enter = function enterVehicle(vehicle) {
-    if (this.db[vehicle.license] != null) { return ErrAlreadyInGarage }
+    if (find(this.db, vehicle.license)) { return ErrAlreadyInGarage }
     let spot = this.levels.reduce((spot, level, floor) => {
         if (spot != null) { return spot }
         let place = level.take()
@@ -23,24 +41,26 @@ Garage.prototype.enter = function enterVehicle(vehicle) {
     }, null)
 
     if (spot == null) { return ErrNoMoreSpace }
-    return this.db[vehicle.license] = { vehicle, spot }
+    let entry = { vehicle, spot }
+    this.db.push(entry)
+    return entry
 }
 
-/** String -> Error | Vehicle */
+/** String -> Either(Error, Vehicle) */
 Garage.prototype.exit = function exitVehicle(license) {
-    let entry = this.db[license]
+    let entry = find(this.db, license)
     if (entry == null) { return ErrNotInGarage }
     let err = this.levels[entry.spot.floor].free(entry.spot.place)
     if (err === ErrIllegalFree) { return ErrNotInGarage }
-    delete this.db[license]
+    remove(this.db, entry)
     return entry.vehicle
 }
 
 /** String -> [Entry] */
-Garage.prototype.list = function listVehicle(pageSize, from) {
-    let keys = Object.keys(this.db)
-    let fromIndex = keys.indexOf(from)
-    return keys.map(k => this.db[k]).slice(fromIndex + 1, fromIndex + pageSize + 1)
+Garage.prototype.list = function listVehicle(pageSize, fromLicense) {
+    let fromIndex = findIndex(this.db, fromLicense)
+    if (fromIndex == null) { fromIndex = -1 }
+    return this.db.slice(fromIndex + 1, fromIndex + pageSize + 1)
 }
 
 // ----- Level
@@ -49,13 +69,13 @@ export function Level(nbSlots) {
     for (var i = 0; i < nbSlots; i += 1) { this.slots.push(nbSlots - i) }
 }
 
-/** () -> Error | Number */
+/** () -> Either(Error, Number) */
 Level.prototype.take = function take() {
     let slot = this.slots.pop()
     return slot == null ? ErrNoMoreSpace : slot
 }
 
-/** Number -> Error | Number */
+/** Number -> Either(Error, Number) */
 Level.prototype.free = function free(slot) {
     let existing = this.slots.indexOf(slot)
     return existing !== -1 ? ErrIllegalFree : this.slots.push(slot)
@@ -66,5 +86,5 @@ export function Vehicle(type, license) {
     this.type = type
     this.license = license
 }
-Vehicle.CAR = "CAR"
-Vehicle.MOTORBIKE = "MOTORBIKE"
+Vehicle.CAR = "Car"
+Vehicle.MOTORBIKE = "Motorbike"
