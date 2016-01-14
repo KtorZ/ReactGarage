645375debddd502acb5e7dad28d29ce37ac85b91
ktorz <matthias.benkort@gmail.com>
2016-01-14 21:54:49 +0100

    Create simple fake navbar

---
+++ frontend/src/containers/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.sass'
+
+export default React.createClass({
+    propTypes: {
+        title: React.PropTypes.string
+    },
+
+    render() {
+        return (
+            <div className='navbar'>
+                <span className='icon'><i className='fa fa-bars'></i></span>
+                <span className='title'>{this.props.title}</span>
+            </div>
+        )
+    }
+})
